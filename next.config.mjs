7c60fa5de139d2810ca396145baa098b60d68d66
AmitDey01 <amitasn2021@gmail.com>
2024-10-20 19:21:38 +0530

    warning fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['13.233.93.31', 'coin-images.coingecko.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'coin-images.coingecko.com',
+      },
+      {
+        protocol: 'http',
+        hostname: '13.233.93.31',
+      },
+    ],
   },
 };
 
