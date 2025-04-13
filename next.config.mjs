f608340996664c87a929623ce3334f76c7b999b0
SHS <santosh12saha@gmail.com>
2025-04-13 15:31:38 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: "http",
         hostname: "crypto-blog.test",
       },
+      {
+        protocol: "https",
+        hostname: "crypto-blog.test",
+      }
     ],
   },
 
