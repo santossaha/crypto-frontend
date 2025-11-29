3dbc4bf611bee4f6e4d64c5e9385bb03f9920335
SHS <santosh12saha@gmail.com>
2025-11-29 13:07:47 +0530

    update

--- src/app/components/HomeSections/HomeSectionsLeft.jsx
+++ src/app/components/HomeSections/HomeSectionsLeft.jsx
@@ -5,21 +5,23 @@ import EventSection from "./EventSection";
 
 const HomeSectionsLeft = ({ news, blogs, events, loading, categories }) => (
   <div className="space-y-8">
-    <NewsSection 
-      news={news} 
-      loading={loading} 
-      categories={categories.news || []} 
-    />
     <BlogSection 
       blogs={blogs} 
       loading={loading} 
       categories={categories.blog || []} 
     />
-    <EventSection 
+    
+    <NewsSection 
+      news={news} 
+      loading={loading} 
+      categories={categories.news || []} 
+    />
+    
+    {/* <EventSection 
       events={events} 
       loading={loading} 
       categories={categories.event || []} 
-    />
+    /> */}
   </div>
 );
 
